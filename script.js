25dc989f5574a73658021b615f20c3324a1899f2
t-toume <taiga1002@gmail.com>
2020-09-25 00:43:24 +0900

    BaseScriptの変数を修正

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var sheet = ss.getSheetByName("シート1");
 function onOpen() {
     var ui = SpreadsheetApp.getUi();
     var menu = ui.createMenu("GAS実行");
-    menu.addItem("メール送信実行_ver1", "sendMergeEmail");
+    menu.addItem("メール送信実行_雛形", "sendMergeEmail");
     menu.addToUi();
 }
 
@@ -20,17 +20,17 @@ function sendMergeEmail(){
 
     var strFrom = sheet.getRange(1,2).getValue();
 
-    var doc_baseID = sheet.getRange(2,2).getValue();
-    var doc_variableID = sheet.getRange(3,2).getValue();
+    var docBaseID = sheet.getRange(2,2).getValue();
+    var docVariableID = sheet.getRange(3,2).getValue();
     
     // 添付ファイル指定がある場合はoptionsに追加（※未使用）
     //var attachementID = sheet.getRange(3,2).getValue();
 
     // テンプレートテキストの取得  
-    var doc_baseTemplate = DocumentApp.openById(doc_baseID);
-    var doc_variableTemplate = DocumentApp.openById(doc_variableID);
-    var str_baseTemplate = doc_baseTemplate.getBody().getText();
-    var str_variableTemplate = doc_variableTemplate.getBody().getText();
+    var docBaseTemplate = DocumentApp.openById(docBaseID);
+    var docVariableTemplate = DocumentApp.openById(docVariableID);
+    var strBaseTemplate = docBaseTemplate.getBody().getText();
+    var strVariableTemplate = docVariableTemplate.getBody().getText();
 
     for (var i = 0; i < data.length; i++) {
         var row = data[i];
@@ -63,7 +63,7 @@ function sendMergeEmail(){
                 var strVal4 = row[6];
                 
                 // メールのbase部分の変数を置換
-                var strBody = str_baseTemplate.replace("\{VALUE1\}",strVal1).replace("\{VALUE2\}",strVal2).replace("\{VALUE3\}",strVal3).replace("\{VALUE4\}",strVal4); 
+                var strBody = strBaseTemplate.replace("\{VALUE1\}",strVal1).replace("\{VALUE2\}",strVal2).replace("\{VALUE3\}",strVal3).replace("\{VALUE4\}",strVal4); 
 
                 // メールのvariable部分の変数を取得
                 var strVal5 = row[7];
@@ -71,7 +71,7 @@ function sendMergeEmail(){
                 var strVal7 = row[9];
                 var strVal8 = row[10];
 
-                var str_variable = str_variableTemplate.replace("\{VALUE5\}",strVal5).replace("\{VALUE6\}",strVal6).replace("\{VALUE7\}",strVal7).replace("\{VALUE8\}",strVal8); 
+                var strVariable = strVariableTemplate.replace("\{VALUE5\}",strVal5).replace("\{VALUE6\}",strVal6).replace("\{VALUE7\}",strVal7).replace("\{VALUE8\}",strVal8); 
 
                 while (data[i+1] != undefined && strTo == data[i+1][0]) {
                     var strVal5 = data[i+1][7];
@@ -79,13 +79,13 @@ function sendMergeEmail(){
                     var strVal7 = data[i+1][9];
                     var strVal8 = data[i+1][10];
 
-                    var str_variable = str_variable + str_variableTemplate.replace("\{VALUE5\}",strVal5).replace("\{VALUE6\}",strVal6).replace("\{VALUE7\}",strVal7).replace("\{VALUE8\}",strVal8); 
+                    var strVariable = strVariable + strVariableTemplate.replace("\{VALUE5\}",strVal5).replace("\{VALUE6\}",strVal6).replace("\{VALUE7\}",strVal7).replace("\{VALUE8\}",strVal8); 
 
                     i = i + 1;
                 }
                 
                 // メールのvariable部分の変数を置換
-                var strBody = strBody.replace("\{VALUE_variable\}",str_variable); 
+                var strBody = strBody.replace("\{VALUE_variable\}",strVariable); 
 
                 // メール送信実行       
                 GmailApp.sendEmail(strTo,strSubject,strBody,options);
