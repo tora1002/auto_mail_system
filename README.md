7755acba6c5d19f10e9ecb377f174bf462178bb9
t-toume <taiga1002@gmail.com>
2020-09-25 00:34:52 +0900

    ベース作成

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # auto_mail_system
 
+## 概要
 
+* ベクトル経理用のメール自動送信システムです
+* Google Spreadsheet + Google Document + Google Apps Script
+** Google Spreadsheet：メールの変数部分を記入
+** Google Document：メールの雛形
+
+## 注意事項
+
+* あくまでメールを送信することを最優先としているため、 __誰に、いつ送ったか？などの履歴を管理するものではない__
 
 ## 参考
-
-* https://note.com/w_yoshida/n/n695f2b91f5a7
-
+* [GASで差し込みメール配信する](https://note.com/w_yoshida/n/n695f2b91f5a7)
+* [Google Apps Script で Spreadsheet にアクセスする方法まとめ](https://qiita.com/negito6/items/c64a7a8589faaffcfdcf)
+* [Google Apps Script を V8 ランタイムで実行する方法](https://qiita.com/tanabee/items/113bfe2e3497b4462b68)
+** マニフェストファイルの表示方法から、V8ランタイムを無効にする
 
 
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var ss = SpreadsheetApp.getActiveSpreadsheet();
-var sheet = ss.getSheets()[0];
+var sheet = ss.getSheetByName("シート1");
 
 // 実行メニューを作成
 function onOpen() {
     var ui = SpreadsheetApp.getUi();
     var menu = ui.createMenu("GAS実行");
-    menu.addItem("メール送信実行", "sendMergeEmail");
+    menu.addItem("メール送信実行_ver1", "sendMergeEmail");
     menu.addToUi();
 }
 
@@ -20,49 +20,72 @@ function sendMergeEmail(){
 
     var strFrom = sheet.getRange(1,2).getValue();
 
-    var docID = sheet.getRange(2,2).getValue();
-    var attachementID = sheet.getRange(3,2).getValue();
+    var doc_baseID = sheet.getRange(2,2).getValue();
+    var doc_variableID = sheet.getRange(3,2).getValue();
+    
+    // 添付ファイル指定がある場合はoptionsに追加（※未使用）
+    //var attachementID = sheet.getRange(3,2).getValue();
 
     // テンプレートテキストの取得  
-    var docTemplate = DocumentApp.openById(docID);
-    var strTemplate = docTemplate.getBody().getText();
+    var doc_baseTemplate = DocumentApp.openById(doc_baseID);
+    var doc_variableTemplate = DocumentApp.openById(doc_variableID);
+    var str_baseTemplate = doc_baseTemplate.getBody().getText();
+    var str_variableTemplate = doc_variableTemplate.getBody().getText();
 
-    for (var i = 0; i < data.length; ++i) {
+    for (var i = 0; i < data.length; i++) {
         var row = data[i];
         row.rowNumber = i + startRow;
 
         // Result列がブランクであれば処理を実行    
-        if (!row[7]) { 
+        if (!row[11]) { 
             var result = "";
 
             try
             {
-                var strVal1 = row[4];
-                var strVal2 = row[5];
-                var strVal3 = row[6];
-                var strVal4 = row[7];
-                var strVal5 = row[8];
-                var strVal6 = row[9];
-                var strVal7 = row[10];
-
-                // テンプレートテキスト内の変数を置換
-                var strBody = strTemplate.replace("\{VALUE1\}",strVal1).replace("\{VALUE2\}",strVal2).replace("\{VALUE3\}",strVal3).replace("\{VALUE4\}",strVal4).replace("\{VALUE5\}",strVal5).replace("\{VALUE6\}",strVal6).replace("\{VALUE7\}",strVal7); 
-
                 var strTo = row[0];
                 var strCc = row[1];
-                var strBcc = row[2];
-                var strSubject = row[3];
+                var strSubject = row[2];
 
                 var options = {};
                 options.cc = strCc;
-                options.bcc = strBcc;
                 options.from = strFrom;
-
+                
                 // 添付ファイル指定がある場合はoptionsに追加（※未使用）
                 //if(attachementID){
                 //    var attachment = DriveApp.getFileById(attachementID);
                 //    options.attachments = attachment
                 //}
+
+                // メールのbase部分の変数を取得
+                var strVal1 = row[3];
+                var strVal2 = row[4];
+                var strVal3 = row[5];
+                var strVal4 = row[6];
+                
+                // メールのbase部分の変数を置換
+                var strBody = str_baseTemplate.replace("\{VALUE1\}",strVal1).replace("\{VALUE2\}",strVal2).replace("\{VALUE3\}",strVal3).replace("\{VALUE4\}",strVal4); 
+
+                // メールのvariable部分の変数を取得
+                var strVal5 = row[7];
+                var strVal6 = row[8];
+                var strVal7 = row[9];
+                var strVal8 = row[10];
+
+                var str_variable = str_variableTemplate.replace("\{VALUE5\}",strVal5).replace("\{VALUE6\}",strVal6).replace("\{VALUE7\}",strVal7).replace("\{VALUE8\}",strVal8); 
+
+                while (data[i+1] != undefined && strTo == data[i+1][0]) {
+                    var strVal5 = data[i+1][7];
+                    var strVal6 = data[i+1][8];
+                    var strVal7 = data[i+1][9];
+                    var strVal8 = data[i+1][10];
+
+                    var str_variable = str_variable + str_variableTemplate.replace("\{VALUE5\}",strVal5).replace("\{VALUE6\}",strVal6).replace("\{VALUE7\}",strVal7).replace("\{VALUE8\}",strVal8); 
+
+                    i = i + 1;
+                }
+                
+                // メールのvariable部分の変数を置換
+                var strBody = strBody.replace("\{VALUE_variable\}",str_variable); 
 
                 // メール送信実行       
                 GmailApp.sendEmail(strTo,strSubject,strBody,options);
